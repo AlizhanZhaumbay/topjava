d1bd803130f62b695eddf2c04150b15470c29396
Alizhan Zhaumbay <moneymanever@gmail.com>
2022-10-22 14:37:17 +0600

    FilterByCycles

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -22,15 +22,31 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        //List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        //mealsTo.forEach(System.out::println);
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        mealsTo.forEach(System.out::println);
 
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        //
-        return null;
+        Map<LocalTime, Integer> caloriesSumByDate = new HashMap<>();
+        for(UserMeal userMeal : meals){
+            LocalTime localTime = userMeal.getDateTime().toLocalTime();
+            caloriesSumByDate.put(localTime,
+                    caloriesSumByDate.getOrDefault(localTime,0) +
+                            userMeal.getCalories());
+        }
+
+        List<UserMealWithExcess> mealsWithExcess = new ArrayList<>();
+        for(UserMeal meal : meals){
+            LocalDateTime localDateTime = meal.getDateTime();
+            if(TimeUtil.isBetweenHalfOpen(localDateTime.toLocalTime(),startTime,endTime)){
+                mealsWithExcess.add(new UserMealWithExcess(localDateTime,meal.getDescription(),
+                        meal.getCalories(),caloriesSumByDate.get(localDateTime.toLocalTime()) < caloriesPerDay));
+            }
+        }
+
+        return mealsWithExcess;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
