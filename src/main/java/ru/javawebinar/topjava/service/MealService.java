7545475e2638efe7e82a85e9b8adc29cf83ae08e
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-01-10 00:42:58 +0500

    Implemented MealService and overloaded method in DateTimeUtil

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,41 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.Collection;
+import java.util.function.Predicate;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    public Meal get(int id, int userId) {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    public void update(Meal meal, int userId){
+        checkNotFoundWithId(repository.save(meal,userId), meal.getId());
+    }
+
+    public Collection<Meal> getAll(int userId, Predicate<Meal> predicate){
+        checkNotFound(userId,"userId=" + userId);
+        return repository.getAll(userId, predicate);
+    }
 }
---
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.util;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtil {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+    }
+
+    public static boolean isBetweenHalfOpen(LocalDateTime lt, LocalDateTime startDate, LocalDateTime endDate){
+        return lt.isAfter(startDate) && lt.isBefore(endDate);
+    }
+
+    public static String toString(LocalDateTime ldt) {
+        return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+}
+
