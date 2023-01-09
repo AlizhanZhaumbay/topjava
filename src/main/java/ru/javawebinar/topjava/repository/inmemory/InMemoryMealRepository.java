3478b2605d0d6675bdbc0a08316f420b177ce189
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-01-10 00:36:41 +0500

    Implemented repos

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,69 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> userMealRepository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(meal, SecurityUtil.authUserId()));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        userMealRepository.putIfAbsent(userId, new ConcurrentHashMap<>());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            meal.setUserId(userId);
+            getByUserId(userId).put(meal.getId(), meal);
             return meal;
         }
+        if (!isOwner(meal.getId(), userId)) return null;
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return getByUserId(userId).computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        if (isOwner(id, userId)) return getByUserId(userId).remove(id) != null;
+        return false;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        if (!isOwner(id, userId)) return null;
+        return userMealRepository.get(userId).get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(int userId, Predicate<Meal> predicate) {
+        return getByUserId(userId).values()
+                .stream()
+                .filter(meal -> meal.getUserId() == userId && predicate.test(meal))
+                .collect(Collectors.toList());
+    }
+
+    private Map<Integer, Meal> getByUserId(int userId) {
+        if (!userMealRepository.containsKey(userId)) return Collections.EMPTY_MAP;
+        return userMealRepository.get(userId);
+    }
+
+    private boolean isOwner(int id, int userId) {
+        Meal optionalMeal = getByUserId(userId).get(id);
+        if (Objects.isNull(optionalMeal)) return false;
+        return optionalMeal.getUserId() == userId;
     }
 }
 
