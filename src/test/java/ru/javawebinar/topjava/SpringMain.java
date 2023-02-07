c7a956c5d27bcdfc78ca2b2d161509ad665eb953
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-02-07 21:16:01 +0600

    fixed_servlet

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.web.SecurityUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
@@ -17,20 +20,10 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-            System.out.println();
-
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
-            System.out.println();
-            System.out.println(mealController.getBetween(null, null, null, null));
-        }
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml", "spring/spring-db.xml"}, false);
+        applicationContext.setConfigLocations("spring/spring-db.xml", "spring/spring-app.xml");
+        applicationContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile());
+        applicationContext.refresh();
     }
 }
