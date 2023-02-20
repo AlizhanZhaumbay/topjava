6493567ad43eca3786ada35553b96e2d73653f82
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-02-20 20:01:32 +0600

    refactored_tests

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,21 +1,8 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.web.SecurityUtil;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
+import ru.javawebinar.topjava.service.UserService;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -23,7 +10,11 @@ public class SpringMain {
         ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml", "spring/spring-db.xml"}, false);
         applicationContext.setConfigLocations("spring/spring-db.xml", "spring/spring-app.xml");
-        applicationContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile());
+        String activeProfile = Profiles.getActiveDbProfile();
+        System.out.println(activeProfile);
+        applicationContext.getEnvironment().setActiveProfiles(activeProfile);
         applicationContext.refresh();
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.getAll().forEach(System.out::println);
     }
 }
