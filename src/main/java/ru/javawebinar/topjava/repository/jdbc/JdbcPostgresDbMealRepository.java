4e4cf0025ffa6432a50b8a95e72692a322fea129
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-02-20 20:02:52 +0600

    fixed_jdbc_repos

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcPostgresDbMealRepository.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+import java.time.LocalDateTime;
+
+import static ru.javawebinar.topjava.Profiles.POSTGRES_DB;
+
+@Repository
+@Profile(POSTGRES_DB)
+public class JdbcPostgresDbMealRepository extends AbstractJdbcMealRepository {
+
+    public JdbcPostgresDbMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    public LocalDateTime getDateTime(LocalDateTime localDateTime) {
+        return localDateTime;
+    }
+}
